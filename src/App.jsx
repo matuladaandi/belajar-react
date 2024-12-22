f64564fc30f44142e22da68b57c382f474fea457
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-12-22 14:13:04 +0800

    initial project react & taiwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center bg-slate-600 min-h-screen items-center">
+        <div>Hello Andi</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
