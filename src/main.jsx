a97551e0c7ead26e2e433d010bfe5b3b72e761ba
matuladaandi@gmail.com <matuladaandi@gmail.com>
2024-12-30 21:59:25 +0800

    imp. routing UI, register & Login Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import LoginPage from "./pages/login";
+import ErrorPage from "./pages/404";
+import RegisterPage from "./pages/register";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello Word</div>,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
