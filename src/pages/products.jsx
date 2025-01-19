409a57a65c6cb828a470be2601b5aa9fbfc9e088
matuladaandi@gmail.com <matuladaandi@gmail.com>
2025-01-19 17:11:08 +0800

    impl: Rendering List

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,22 +1,36 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+  {
+    id: 1,
+    name: "kursi tua",
+    price: 99,
+    image: "/images/kursi-tua.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, possimus dolores? Mollitia distinctio error a",
+  },
+  {
+    id: 2,
+    name: "kursi baru",
+    price: 127,
+    image: "/images/kursi-baru.jpg",
+    description:
+      "Lorem ipsum dolor sit amet consectetur",
+  },
+];
+
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct>
-        <CardProduct.Header image="/images/kursi-tua.jpg" />
-        <CardProduct.Body title="kursi tua">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae quas, perspiciatis blanditiis ad ipsam impedit!
-        </CardProduct.Body>
-        <CardProduct.Footer price="99k" />
-      </CardProduct>
-      <CardProduct>
-        <CardProduct.Header image="/images/kursi-tua.jpg" />
-        <CardProduct.Body title="kursi Baru">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit
-        </CardProduct.Body>
-        <CardProduct.Footer price="712k" />
-      </CardProduct>
+      {products.map((product) => (
+        <CardProduct key={product.id}>
+          <CardProduct.Header image={product.image} />
+          <CardProduct.Body name={product.name}>
+            {product.description}
+          </CardProduct.Body>
+          <CardProduct.Footer price={product.price} />
+        </CardProduct>
+      ))}
     </div>
   );
 };
